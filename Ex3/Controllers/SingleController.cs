04b957abc75a7a65643ad1fb0702321ef569d9b1
noamspok <noamspok@DESKTOP-8FA45J9>
2017-06-20 19:09:15 +0300

    bla bla

---
+++ Ex3/Controllers/SingleController.cs
@@ -0,0 +1,45 @@
+﻿using Ex3.Models;
+using MazeLib;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Ex3.Controllers
+{
+    public class SingleController : ApiController
+    {
+        private SingleModel singleModel = new SingleModel();
+        // GET: api/Single
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Single/5
+        public JObject Get(string name, int rows, int cols)
+        {
+            Maze maze = singleModel.GenerateMaze(name, rows, cols);
+            JObject obj = JObject.Parse(maze.ToJSON());
+            return obj;
+        }
+
+        // POST: api/Single
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Single/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Single/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Ex3/Models/SingleModel.cs
@@ -0,0 +1,64 @@
+﻿using MazeGeneratorLib;
+using MazeLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SearchAlgorithmsLib;
+
+namespace Ex3.Models
+{
+    public class SingleModel
+    {
+        private DFSMazeGenerator dFSMazeGenerator;
+        private static Dictionary<string, Maze> mazes;
+
+
+        /// <summary>
+        /// Generates the maze.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="rows">The amount of rows.</param>
+        /// <param name="cols">The amount of columns.</param>
+        /// <returns>Maze.</returns>
+        public Maze GenerateMaze(string name, int rows, int cols)
+        {
+            Maze maze = this.dFSMazeGenerator.Generate(rows, cols);
+            maze.Name = name;
+           mazes.Add(name, maze);
+            return maze;
+        }
+
+        /// <summary>
+        /// Solves the path for given maze
+        /// </summary>
+        /// <param name="name">The maze</param>
+        /// <param name="algorithm">The algorithm to solve with.</param>
+        /// <returns>the path</returns>
+        public string Solve(string name, int algorithm)
+        {
+            //if (this.solutions.ContainsKey(name))
+            //{
+            //    return this.solutions[name];
+            //}
+            Solution<Position> solution = null;
+            Adapter adapted = new Adapter(mazes[name]);
+            ISearcher<Position> searcher = null;
+            switch (algorithm)
+            {
+                case 0:
+                    searcher = new BFS<Position>();
+                    break;
+                case 1:
+                    searcher = new DFS<Position>();
+                    break;
+            }
+            solution = searcher.Search(adapted);
+            SolveSolution solveSolution = new SolveSolution(name, solution, searcher.GetNumberOfNodesEvaluated());
+            string toJSON = solveSolution.ToJSON();
+            //solutions.Add(name, toJSON);
+            return toJSON;
+        }
+
+    }
+}
