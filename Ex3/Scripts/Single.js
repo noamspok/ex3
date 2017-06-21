02cd769edf445b9a0b2bfe132f2505fd3acaf1ab
יהושע ליפסקר <yeholipsker@gmail.com>
2017-06-22 00:35:28 +0300

    advance in single plyer

--- Ex3/Scripts/Single.js
+++ Ex3/Scripts/Single.js
@@ -1,4 +1,10 @@
-﻿$("#StrtBtn").click(function () {
+﻿/*$(document).ready(function () {
+
+var script = document.createElement('script');
+script.src = './Scripts/jquery.mazeBoard.js';
+document.getElementsByTagName('head')[0].appendChild(script);*/
+
+$("#StrtBtn").click(function () {
     var apiUrl = "../api/Single/" + $("#mazeName").val() + "/" + $("#rows").val() + "/" + $("#cols").val();
     /*$.getJSON(apiUrl)*/$.ajax({
         method: "GET",
@@ -11,13 +17,7 @@
         var startCol = maze.Start.Col;
         var exitRow = maze.End.Row;
         var exitCol = maze.End.Col;
-        alert(rows);
-        alert(cols);
-        alert(startRow);
-        alert(startCol);
-        alert(exitRow);
-        alert(exitCol);
-        alert(maze);
+
         var mazeData = new Array(rows);
         for (var i = 0; i < rows; i++) {
             mazeData[i] = new Array(cols);
@@ -31,7 +31,9 @@
             }
         }
 
-        $("#mazeCanvas").mazeBoard(mazeData, startRow, startCol, exitRow, exitCol);
+        var mazeBoard = $("#mazeCanvas").mazeBoard(mazeData, startRow, startCol, exitRow, exitCol);
     });
 });
+
+/*});*/
 //{"Name":"my","Maze":"0001001110000101101000000","Rows":5,"Cols":5,"Start":{"Row":0,"Col":2},"End":{"Row":2,"Col":2}}
--- Ex3/Scripts/jQuery.mazeBoard.js
+++ Ex3/Scripts/jQuery.mazeBoard.js
@@ -1,109 +1,107 @@
 ﻿(function ($) {
-	var canvas;
-	var context;
-	var rows;
-	var cols;
-	var cellWidth;
-	var cellHeight;
-	var mazeDat;
-	var strRow;
-	var strCol;
-	var xitRow;
-	var xitCol;
-	var playerImg = document.getElementById(" ../Images / Mario.png");
-	var exitImg=document.getElementById(" ../Images / exit.jpg");
-	var playerRowLoc;
-	var playerColLoc;
+    var canvas;
+    var context;
+    var rows;
+    var cols;
+    var cellWidth;
+    var cellHeight;
+    var mazeDat;
+    var strRow;
+    var strCol;
+    var xitRow;
+    var xitCol;
+    var playerImg = new Image();
+    playerImg.src = "../Images/Mario.png";
+    var exitImg = new Image();
+    exitImg.src = "../Images/exit.jpg";
+    var playerRowLoc;
+    var playerColLoc;
 
-	$.fn.mazeBoard = function (mazeData, startRow, startCol, exitRow, exitCol) {
-		canvas = $(this)[0];
-		context = canvas.getContext("2d");
-		rows = mazeData.length;
-		cols = mazeData[0].length;
-		cellWidth = mazeCanvas.width / cols;
-		cellHeight = mazeCanvas.height / rows;
-		mazeDat = mazeData;
-		strRow = startRow;
-		strCol = startCol;
-		xitRow = exitRow;
-		xitCol = exitCol;
-		playerImg = playerIamge;
-		exitImg = exitImage;
-		playerRowLoc = strRow;
-		playerColLoc=strCol;
+    $.fn.mazeBoard = function (mazeData, startRow, startCol, exitRow, exitCol) {
+        canvas = $(this)[0];
+        context = canvas.getContext("2d");
+        rows = mazeData.length;
+        cols = mazeData[0].length;
+        cellWidth = mazeCanvas.width / cols;
+        cellHeight = mazeCanvas.height / rows;
+        mazeDat = mazeData;
+        strRow = startRow;
+        strCol = startCol;
+        xitRow = exitRow;
+        xitCol = exitCol;
+        playerImg = playerImg;
+        exitImg = exitImg;
+        playerRowLoc = strRow;
+        playerColLoc = strCol;
 
-		for (var i = 0; i < rows; i++) {
-			for (var j = 0; j < cols; j++) {
-				if (mazeData[i][j] == 1) {
-					context.fillRect(cellWidth * j, cellHeight * i,
-						cellWidth, cellHeight);
-				}
-			}
-			context.fillRect(playerRowLoc, playerColLoc, cellWidth, cellHeight);
-			context.drawImage(exitImg, xitRow, xitCol, cellWidth, cellHeight);
-			document.addEventListener("onkeydown", move, false);
-			
-		}
+        for (var i = 0; i < rows; i++) {
+            for (var j = 0; j < cols; j++) {
+                if (mazeData[i][j] == 1) {
+                    context.fillRect(cellWidth * j, cellHeight * i,
+                        cellWidth, cellHeight);
+                }
+            }
+        }
+        context.drawImage(playerImg, playerRowLoc, playerColLoc, cellWidth, cellHeight);
+        context.drawImage(exitImg, xitRow, xitCol, cellWidth, cellHeight);
+        document.addEventListener("onkeydown", move, false);
+    }
+    function movePlayer(newRowLoc, newColLoc) {
+        context.fillStyle = "#FF0000";
+        context.fillRect(playerRowLoc, playerColLoc, cellWidth, cellHeight);
+        context.drawImage(playerImg, newRowLoc, newColLoc, cellWidth, cellHeight);
+    }
+    function move(event) {
 
+        switch (event.keyCode) {
+            case 37:
+                if (mazeDat[playerRowLoc][playerColLoc - 1] == 0) {
+                    move(playerRowLoc, playerColLoc - 1);
+                }
 
+                break;
+            case 38:
+                if (mazeDat[playerRowLoc - 1][playerColLoc] == 0) {
+                    move(playerRowLoc - 1, playerColLoc);
+                }
+                break;
+            case 39:
+                if (mazeDat[playerRowLoc][playerColLoc + 1] == 0) {
+                    move(playerRowLoc, playerColLoc + 1);
+                }
+                break;
+            case 40:
+                if (mazeDat[playerRowLoc + 1][playerColLoc] == 0) {
+                    move(playerRowLoc + 1, playerColLoc);
+                }
+                break;
+        }
+        if (playerRowLoc == xitRow && playerColLoc == xitCol) {
 
-	}
-	function movePlayer(newRowLoc, newColLoc) {
-		context.fillStyle = "#FF0000";
-		context.fillRect(playerRowLoc, playerColLoc, cellWidth, cellHeight);
-		context.drawImage(playerImg, newRowLoc, newColLoc, cellWidth, cellHeight);
-	}
-	function move(event) {
+        }
 
-		switch (event.keyCode) {
-			case 37:
-				if (mazeDat[playerRowLoc][playerColLoc - 1] == 0) {
-				    move(playerRowLoc, playerColLoc - 1);
-				}
+    }
+    $.fn.solveMaze = function (solution) {
+        movePlayer(strRow, strCol);
+        var i = 0;
+        var x = setInterval(function () {
 
-				break;
-			case 38:
-				if (mazeDat[playerRowLoc -1][playerColLoc ] == 0) {
-				    move(playerRowLoc - 1, playerColLoc);
-				}
-				break;
-			case 39:
-				if (mazeDat[playerRowLoc][playerColLoc + 1] == 0) {
-				    move(playerRowLoc, playerColLoc + 1);
-				}
-				break;
-			case 40:
-				if (mazeDat[playerRowLoc + 1][playerColLoc] == 0) {
-				    move(playerRowLoc + 1, playerColLoc);
-				}
-				break;
-		}
-		if (playerRowLoc == xitRow && playerColLoc == xitCol) {
-
-		}
-
-	}
-	$.fn.solveMaze = function (solution) {
-	    movePlayer(strRow, strCol);
-	    var i=0;
-	    var x = setInterval(function () {
-
-	        switch (solution[i]) {
-	            case "0":
-	                move(playerRowLoc, playerColLoc - 1);
-	                break;
-	            case '1':
-	                move(playerRowLoc, playerColLoc + 1);
-	                break;
-	            case '2':
-	                move(playerRowLoc - 1, playerColLoc);
-	                break;
-	            case '3':
-	                move(playerRowLoc + 1, playerColLoc);
-	                break;
-	        }
+            switch (solution[i]) {
+                case "0":
+                    move(playerRowLoc, playerColLoc - 1);
+                    break;
+                case '1':
+                    move(playerRowLoc, playerColLoc + 1);
+                    break;
+                case '2':
+                    move(playerRowLoc - 1, playerColLoc);
+                    break;
+                case '3':
+                    move(playerRowLoc + 1, playerColLoc);
+                    break;
+            }
             i++
-	    }, 20);
-	}
+        }, 20);
+    }
 
-})(jQuery);
+})(jQuery); 
